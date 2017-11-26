0241c61bb5cba7c21ccdc1f2e6c8470c4c8cdef2
DELL <250512412@qq.com>
2017-11-26 17:16:57 +0800

    mybatis/log4j

---
+++ src/main/java/cn/zjc/autotask/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package cn.zjc.autotask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by zhangjiacheng on 2017/11/26
+ */
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    @Scheduled(cron="0 0/1 8-20 * * ?")
+    public void autoTask(){
+        System.out.println("开始自动任务");
+        log.info("开始自动任务");
+    }
+}
