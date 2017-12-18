a7fdbc8ed1bd630e330452399e10a727902b4b88
zhangjiacheng <zhangjiacheng>
2017-12-18 14:30:13 +0800

    mq

--- src/main/java/cn/zjc/controller/user/UserController.java
+++ src/main/java/cn/zjc/controller/user/UserController.java
@@ -1,6 +1,5 @@
 package cn.zjc.controller.user;
 
-//import cn.zjc.esdao.user.UserSearchRepository;
 import cn.zjc.esdao.user.UserSearchRepository;
 import cn.zjc.model.user.User;
 import cn.zjc.server.user.UserService;
@@ -8,16 +7,14 @@ import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Iterator;
 
 /**
  * Created by zhangjiacheng on 2017/11/16.
  */
-@Controller
+@RestController
 @RequestMapping(value = "/user")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
@@ -28,15 +25,13 @@ public class UserController {
     private UserSearchRepository userSearchRepository;
 
     @RequestMapping(value = "/all")
-    @ResponseBody
     public Object findAllUser(){
         User user = new User();
         System.out.println("===========");
         log.info("===========");
-        return userService.selectPageAll(2,2,user);
+        return userService.selectPageAll(1,15,user);
     }
     @RequestMapping(value = "/add")
-    @ResponseBody
     public String addUser(){
         User user = new User();
         log.info("===========");
@@ -48,7 +43,6 @@ public class UserController {
         return "success";
     }
     @RequestMapping("/query")
-    @ResponseBody
     public void testSearch() {
         String queryString = "springboot";//搜索关键字
         QueryStringQueryBuilder builder = new QueryStringQueryBuilder(queryString);
