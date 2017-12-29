ee50f0a99727f1cc9f3542a9813a1df9fcfb7d83
zhangjiacheng <zhangjiacheng>
2017-12-29 18:00:20 +0800

    es

--- src/main/java/cn/zjc/config/ElasticsearchClient.java
+++ src/main/java/cn/zjc/config/ElasticsearchClient.java
@@ -83,8 +83,10 @@ public class ElasticsearchClient implements FactoryBean<TransportClient>, Initia
      */
     private Settings settings(){
         Settings settings = Settings.builder()
-                .put("cluster.name",clusterName)//设置ES实例的名称
-                .put("client.transport.sniff",true)//自动嗅探整个集群的状态，把集群中其他ES节点的ip添加到本地的客户端列表中
+                //设置ES实例的名称
+                .put("cluster.name",clusterName)
+                //自动嗅探整个集群的状态，把集群中其他ES节点的ip添加到本地的客户端列表中
+                .put("client.transport.sniff",true)
                 .build();
         transportClient = new PreBuiltTransportClient(settings);
         return settings;
