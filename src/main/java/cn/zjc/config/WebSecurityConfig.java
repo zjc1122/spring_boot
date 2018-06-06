1f8beafdee851914e610fac0852452f05463dfa3
zhangjiacheng <zjc_1122@163.com>
2018-06-06 21:55:39 +0800

    调整

--- src/main/java/cn/zjc/config/WebSecurityConfig.java
+++ src/main/java/cn/zjc/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
@@ -26,11 +27,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import javax.annotation.Resource;
 
+/**
+ * @EnableWebSecurity: 禁用Boot的默认Security配置，配合@Configuration启用自定义配置（需要扩展WebSecurityConfigurerAdapter）
+ * @EnableGlobalMethodSecurity(prePostEnabled = true): 启用Security注解，例如最常用的@PreAuthorize
+ * configure(HttpSecurity): Request层面的配置，对应XML Configuration中的<http>元素
+ * configure(WebSecurity): Web层面的配置，一般用来配置无需安全检查的路径
+ * configure(AuthenticationManagerBuilder): 身份验证配置，用于注入自定义身份验证Bean和密码校验规则
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 2 * 60)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Resource
     private SessionRegistry sessionRegistry;
@@ -73,6 +81,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and().httpBasic();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**", "/**/favicon.ico");
+    }
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
