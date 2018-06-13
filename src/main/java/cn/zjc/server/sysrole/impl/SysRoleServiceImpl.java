b0bbe2b4958a13adab844ccc7b54f3d5782ec00b
zhangjiacheng <zjc_1122@163.com>
2018-06-13 12:16:56 +0800

    update

--- src/main/java/cn/zjc/server/sysrole/impl/SysRoleServiceImpl.java
+++ src/main/java/cn/zjc/server/sysrole/impl/SysRoleServiceImpl.java
@@ -6,7 +6,6 @@ import cn.zjc.server.base.impl.BaseServerImpl;
 import cn.zjc.server.sysrole.SysRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,14 +13,13 @@ import java.util.List;
  * Created by zhangjiacheng on 2018/2/2.
  */
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class SysRoleServiceImpl extends BaseServerImpl<SysRole> implements SysRoleService {
 
     @Autowired
     private SysRoleMapper sysRoleMapper;
 
     @Autowired
-    public void setBaseMapper(){
+    public void setBaseMapper() {
         super.setBaseMapper(sysRoleMapper);
     }
 
--- src/main/java/cn/zjc/server/user/impl/UserServiceImpl.java
+++ src/main/java/cn/zjc/server/user/impl/UserServiceImpl.java
@@ -6,13 +6,11 @@ import cn.zjc.server.base.impl.BaseServerImpl;
 import cn.zjc.server.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by zhangjiacheng on 2017/11/15.
  */
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class UserServiceImpl extends BaseServerImpl<User> implements UserService {
 
     @Autowired
