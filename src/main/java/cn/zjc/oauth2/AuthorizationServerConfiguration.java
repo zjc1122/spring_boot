90eaaff4bb6ff8724cf4eb8156c2d85ebdf17b1f
zhangjiacheng <zjc_1122@163.com>
2018-07-05 18:29:15 +0800

    OAuth2.0

--- src/main/java/cn/zjc/oauth2/AuthorizationServerConfiguration.java
+++ src/main/java/cn/zjc/oauth2/AuthorizationServerConfiguration.java
@@ -1,125 +1,83 @@
 //package cn.zjc.oauth2;
 //
-//import cn.zjc.oauth2.ApplyClientDetailService;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 //import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.bind.RelaxedPropertyResolver;
-//import org.springframework.context.EnvironmentAware;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.context.annotation.Primary;
 //import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 //import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 //import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 //import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.ClientDetailsService;
+//import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 //import org.springframework.security.oauth2.provider.token.TokenStore;
-//import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 //import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-//import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 //
-//import javax.annotation.Resource;
 //import javax.sql.DataSource;
+//import java.util.concurrent.TimeUnit;
 //
 ///**
-// * @author : zhangjiacheng
-// * @ClassName : AuthorizationServerConfiguration
-// * @date : 2018/7/4
-// * @Description : 配置OAuth2验证服务器
+// * @author zjc
 // */
 //@Configuration
 //@EnableAuthorizationServer
-//public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter implements EnvironmentAware {
+//public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 //
-//    private static final Logger logger = LoggerFactory.getLogger(AuthorizationServerConfiguration.class);
-//
-//    private static final String ENV_OAUTH = "authentication.oauth.";
-//    private static final String PROP_CLIENT_ID = "clientId";
-//    private static final String PROP_SECRET = "secret";
-//    private static final String PROP_TOKEN_VALIDITY_SECONDS = "tokenValidityInSeconds";
-//
-//    private RelaxedPropertyResolver propertyResolver;
-//
-//    @Resource
-//    RedisConnectionFactory redisConnectionFactory;
 //    @Autowired
 //    private DataSource dataSource;
-//    @Resource
-//    private AuthenticationManager authenticationManager;
 //
-//    /**
-//     * 初始化JdbcTokenStore
-//     * 基于JDBC的实现，令牌（Access Token）会保存到数据库
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public TokenStore getTokenStore() {
+//    @Bean // 声明TokenStore实现
+//    public TokenStore tokenStore() {
 //        return new JdbcTokenStore(dataSource);
 //    }
 //
-//    /**
-//     * 初始化RedisTokenStore
-//     * 基于Redis的实现，令牌（Access Token）会保存到Redis
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public TokenStore getRedisTokenStore() {
-//        return new RedisTokenStore(redisConnectionFactory);
+//    @Bean // 声明 ClientDetails实现
+//    public ClientDetailsService clientDetails() {
+//        return new JdbcClientDetailsService(dataSource);
 //    }
 //
-//    /**
-//     * 使用内存中的 token store
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public TokenStore getMemoryTokenStore() {
-//        return new InMemoryTokenStore();
-//    }
+//    @Autowired
+//    private TokenStore tokenStore;
 //
-//    public ApplyClientDetailService getClientDetails() {
-//        return new ApplyClientDetailService();
-//    }
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
 //
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-//        endpoints.tokenStore(getRedisTokenStore())
-//                .authenticationManager(authenticationManager);
-//    }
+//    @Autowired
+//    private UserDetailsService userService;
+//
+//    @Autowired
+//    private ClientDetailsService clientDetails;
 //
 //    @Override
 //    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        // 配置客户端, 用于client认证
-//        clients.withClientDetails(getClientDetails());
-//        //使用存在内存中配置
-////        clients.inMemory()
-////                //client_id用来标识客户的Id
-////                .withClient(propertyResolver.getProperty(PROP_CLIENT_ID))
-////                //允许授权范围
-////                .scopes("read", "write")
-////                //客户端可以使用的权限
-////                .authorities("ROLE_ADMIN", "ROLE_USER")
-////                //允许授权类型
-////                .authorizedGrantTypes("password", "refresh_token")
-////                //secret客户端安全码
-////                .secret(propertyResolver.getProperty(PROP_SECRET))
-////                .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
+//        clients.jdbc(dataSource);
 //    }
 //
 //    @Override
-//    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
-//        //允许表单认证
-//        oauthServer.allowFormAuthenticationForClients();
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.authenticationManager(authenticationManager);
+//        endpoints.tokenStore(tokenStore());
+//        endpoints.userDetailsService(userService);
+//        endpoints.setClientDetailsService(clientDetails);
+//        //配置TokenServices参数
+//        DefaultTokenServices tokenServices = new DefaultTokenServices();
+//        tokenServices.setTokenStore(endpoints.getTokenStore());
+//        tokenServices.setSupportRefreshToken(true);
+//        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+//        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+//        tokenServices.setAccessTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(1)); // 1天
+//        endpoints.tokenServices(tokenServices);
 //    }
 //
-//    @Override
-//    public void setEnvironment(Environment environment) {
-//        //获取到前缀是"authentication.oauth." 的属性列表值.
-//        this.propertyResolver = new RelaxedPropertyResolver(environment, ENV_OAUTH);
+//    @Bean
+//    @Primary
+//    public DefaultTokenServices tokenServices() {
+//        DefaultTokenServices tokenServices = new DefaultTokenServices();
+//        tokenServices.setSupportRefreshToken(true);
+//        tokenServices.setTokenStore(tokenStore);
+//        return tokenServices;
 //    }
 //}
--- src/main/java/cn/zjc/oauth2/ResourceServerConfiguration.java
+++ src/main/java/cn/zjc/oauth2/ResourceServerConfiguration.java
@@ -1,5 +1,5 @@
 //package cn.zjc.oauth2;
-//
+
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 //import org.springframework.context.annotation.Bean;
@@ -11,13 +11,13 @@
 //import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 //
 //import javax.annotation.Resource;
-//
+
 //@Configuration
 //@EnableResourceServer
 //public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 //
 //    private Logger logger = LoggerFactory.getLogger(ResourceServerConfiguration.class);
-//
+
 //    @Resource
 //    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint ;
 //
@@ -40,18 +40,24 @@
 ////                    .antMatchers("/product/**").access("#oauth2.hasScope('select') and hasRole('ROLE_USER')")
 //                .antMatchers("/**").authenticated();  //配置访问权限控制，必须认证过后才可以访问
 //    }
-////    @Override
-////    public void configure(HttpSecurity http) throws Exception {
-////        logger.info("=========================111111111=========");
-////       http.exceptionHandling()
-////               .authenticationEntryPoint(customAuthenticationEntryPoint)
-////               .and()
-////               .logout()
-////               .logoutUrl("/oauth/logout")
-////               .logoutSuccessHandler(customLogoutSuccessHandler())
-////               .and()
-////               .authorizeRequests()
-////               .antMatchers("/hello/").permitAll()
-////               .antMatchers("/secure/**").authenticated();
-////    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        logger.info("=========================111111111=========");
+//       http.exceptionHandling()
+//               .authenticationEntryPoint(customAuthenticationEntryPoint)
+//               .and()
+//               .logout()
+//               .logoutUrl("/oauth/logout")
+//               .logoutSuccessHandler(customLogoutSuccessHandler())
+//               .and()
+//               .authorizeRequests()
+//               .antMatchers("/hello/").permitAll()
+//               .antMatchers("/secure/**").authenticated();
+//    }
+
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/**").authenticated()
+//                .anyRequest().authenticated();
+//    }
 //}
