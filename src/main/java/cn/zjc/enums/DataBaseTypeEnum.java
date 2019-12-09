cee5c2767d24c45d69c91b44c4d2f662ced4fcf9
zhangjiacheng <zjc_1122@163.com>
2019-12-09 17:40:28 +0800

    update

--- src/main/java/cn/zjc/enums/DataBaseTypeEnum.java
+++ src/main/java/cn/zjc/enums/DataBaseTypeEnum.java
@@ -3,13 +3,13 @@ package cn.zjc.enums;
 /**
  * Created by zhangjiacheng on 2017/12/14.
  */
-public enum DataBaseType {
+public enum DataBaseTypeEnum {
 
     Default_DB("defaultDataSource"),
     Slave_DB("slaveDataSource");
     private String dbName;
 
-    DataBaseType(String dbName) {
+    DataBaseTypeEnum(String dbName) {
         this.dbName = dbName;
     }
     public String get(){
